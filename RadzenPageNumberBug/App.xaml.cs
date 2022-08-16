c76e5073c7722a734f0360b86e6e934af20aa50c
Julian Dormon <julian@proofpix.com>
2022-08-16 15:38:14 -0500

    Initial

---
+++ RadzenPageNumberBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RadzenPageNumberBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ RadzenPageNumberBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RadzenPageNumberBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
